144865aa62802844881d36d64091cf4c20669f69
Shiwendra <shiwendra@gmail.com>
2026-07-11 15:49:03 +0530

    upadted codes

--- Python/python1.py
+++ Python/python1.py
@@ -1,1 +1,7 @@
-print('Hello, World!')
+MAB=5000
+cibil_score=750
+ACC_balance=10000
+if MAB>5000 and cibil_score>700 and ACC_balance>5000:
+    print("Eligible for loan")  
+else:
+    print("Not eligible for loan,Because either MAB is less than or equal to 5000, cibil_score is less than or equal to 700, or ACC_balance is less than or equal to 5000.")
